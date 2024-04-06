bf06790227f124488205408f7ba8f1c023004569
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-06 08:18:23 -0300

    Adicionar arquivos de projeto.

---
+++ SintaxeAlternativa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double preco = double.Parse(Console.ReadLine(), CI);
+
+                double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+                
+     
+                Console.WriteLine(desconto);
+
+            }
+        }
+    }
+}
